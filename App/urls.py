a30b3ab5ba03c769c4cea91d166302d4d7e4d5ae
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-08-06 19:15:25 +0100

    all views set up

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import home
+from .views import (
+    browse, buy, item, home, portfolio, settings,
+    watchlist, trending
+)
 
 urlpatterns = [
-    path('', home.home, name='home')
+    path('', home.home, name='home'),
+    path('browse/', browse.browse, name='browse'),
+    path('buy/', buy.buy, name='buy'),
+    path('item/<str:item_id>', item.item, name='item'),
+    path('portfolio/', portfolio.portfolio, name='portfolio'),
+    path('settings/', settings.settings, name='settings'),
+    path('watchlist/', watchlist.watchlist, name='watchlist'),
+    path('trending/', trending.trending, name='trending'),
 ]
