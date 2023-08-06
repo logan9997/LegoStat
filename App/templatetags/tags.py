bd6ce868044b1f8f2026a07621e28ad1e50fdd17
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-08-06 19:04:20 +0100

    template tags created, tags to return list of item ids and names created

---
+++ App/templatetags/tags.py
@@ -0,0 +1,15 @@
+from django import template
+from ..models import Item
+from utils import clean_html_codes
+
+register = template.Library()
+
+@register.simple_tag
+def get_all_item_ids():
+    return list(Item.objects.all().values_list('item_id', flat=True))
+
+@register.simple_tag
+def get_all_item_names():
+    names = list(Item.objects.all().values_list('item_name', flat=True))
+    names = list(map(clean_html_codes, names))
+    return names
