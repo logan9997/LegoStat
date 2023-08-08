8bae70f218c23f44180c06eac581cf0cd6754d77
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-08-08 20:41:40 +0100

    all information from models passed into dict

--- App/views/item.py
+++ App/views/item.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from ..models import Item, Price
+from config import Date
+from datetime import datetime as dt
 
-def item(request):
+def item(request, item_id:str):
 
-    context = {}
+    price_table_filters = {'item_id':item_id, 'date':dt.today().strftime(Date.DATE_FORMAT)}
+
+    item_info = Item.objects.filter(item_id=item_id, price__date=dt.today(
+        ).strftime(Date.DATE_FORMAT)).values(
+            'item_id', 'item_name', 'year_released', 'item_type', 'views',
+            'price__date', 'price__avg_price', 'price__total_quantity',
+            'price__condition'
+        ).annotate(
+        price_new=Price.objects.filter(**price_table_filters, condition='N'
+            ).values_list('avg_price', flat=True),    
+        price_used=Price.objects.filter(**price_table_filters, condition='U'
+            ).values_list('avg_price', flat=True),
+        quantity_new=Price.objects.filter(**price_table_filters, condition='N'
+            ).values_list('total_quantity', flat=True),
+        quantity_used=Price.objects.filter(**price_table_filters, condition='U'
+            ).values_list('total_quantity', flat=True),
+    )[0]
+
+    context = {
+        'item_info':item_info
+    }
 
     return render(request, 'App/item.html', context=context)
