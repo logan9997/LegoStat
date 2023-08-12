aa10a1fb586aca0318d607521079a2e188e21743
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-08-12 11:20:04 +0100

    created timer wrapper function

--- packages/utils/build/lib/utils.py
+++ packages/utils/build/lib/utils.py
@@ -8,3 +8,8 @@ def clean_html_codes(string:str):
         if k in string:
             string = string.replace(k, v)
     return string
+
+def item_type_convert(item_type:str):
+    if len(item_type) > 1:
+        return {'MINIFIG':'M', 'SET':'S'}[item_type]
+    return {'M':'MINIFIG', 'S':'SET'}[item_type]
--- packages/utils/utils.py
+++ packages/utils/utils.py
@@ -1,3 +1,5 @@
+import time
+
 def clean_html_codes(string:str):
     codes = {
         '&#40;':'(',
@@ -8,3 +10,18 @@ def clean_html_codes(string:str):
         if k in string:
             string = string.replace(k, v)
     return string
+
+def item_type_convert(item_type:str):
+    if len(item_type) > 1:
+        return {'MINIFIG':'M', 'SET':'S'}[item_type]
+    return {'M':'MINIFIG', 'S':'SET'}[item_type]
+
+
+def timer(func):
+    def inner(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        finish_time = time.time() - start_time
+        print(f'<{func.__name__}> Finished in {round(finish_time)} seconds.')
+    return inner
+    
