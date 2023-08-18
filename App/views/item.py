893bd0b2804d2b9e09acc5c67a70617e23527b20
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-08-18 22:38:39 +0100

    more fields annotated to item_info : in_watchlist, in_portfolio, portfolio_value

--- App/views/item.py
+++ App/views/item.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from ..models import Item, Price
-from django.db.models import Max
+from ..models import Item, Price, Watchlist, Portfolio
+from django.db.models import Max, Exists, Count, Subquery, OuterRef, Value, Case, F, FloatField, Sum, Value, When
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models.functions import Coalesce
+from config import Options
 
-def item(request, item_id:str):
+
+def item(request:WSGIRequest, item_id:str):
+
+    user_id = request.session.get('user_id', -1)
 
     max_date = Price.objects.filter(item_id=item_id).aggregate(max_date=Max('date')).get('max_date')
     price_table_filters = {'item_id':item_id, 'date':max_date}
@@ -20,6 +26,26 @@ def item(request, item_id:str):
             ).values_list('total_quantity', flat=True),
         quantity_used=Price.objects.filter(**price_table_filters, condition='U'
             ).values_list('total_quantity', flat=True),
+        in_watchlist=Exists(Watchlist.objects.filter(
+            user_id=user_id,item_id=item_id)),
+        portfolio_quantity=Coalesce(
+            Subquery(Portfolio.objects.filter(
+                user_id=user_id, item_id=OuterRef('item_id')
+            ).values(c=Count('item_id'))[:1]),
+            Value(0)
+        ),
+        portfolio_value=F('portfolio_quantity') * (
+            Case(
+                When(price__condition='N', then=F('price_new')),
+                default=Value(0),
+                output_field=FloatField()
+            ) +
+            Case(
+                When(price__condition='U', then=F('price_used')),
+                default=Value(0),
+                output_field=FloatField()
+    )
+)
     )[0]
 
     item_info['graph_prices_new'] = list(Price.objects.filter(item_id=item_id, condition='N').values_list('avg_price', flat=True))
@@ -31,7 +57,11 @@ def item(request, item_id:str):
     item_info['graph_dates'] = list(Price.objects.filter(item_id=item_id, condition='N').values_list('date', flat=True))
 
     context = {
-        'item_info':item_info
+        'item_info':item_info,
+        'graph_metrics':Options.GRAPH_METRICS,
     }
 
     return render(request, 'App/item.html', context=context)
+
+
+
