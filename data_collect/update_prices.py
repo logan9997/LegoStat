90f65a2b7f63c29db546f058ef0eff5f398043ca
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-08-18 22:41:15 +0100

    gets prices for new and used items, timer wrapper added

--- data_collect/update_prices.py
+++ data_collect/update_prices.py
@@ -1,6 +1,6 @@
 from responses import Response
 from db import DB
-from pprint import pprint
+from utils import item_type_convert, timer
 
 class Update:
 
@@ -8,9 +8,9 @@ class Update:
         self.db = DB()
         self.response = Response()
 
-    def get_price_data(self, item_id:str, condition:str) -> dict:
+    def get_price_data(self, item_id:str, condition:str, item_type:str) -> dict:
         data = self.response.get_response(
-            f'items/MINIFIG/{item_id}/price?new_or_used={condition}'
+            f'items/{item_type}/{item_id}/price?new_or_used={condition}'
         )
         return data
     
@@ -20,10 +20,17 @@ class Update:
     def parse_ids(self):
         self.ids = self.db.get_ids()
         for item_id in self.ids:
+            item_type = self.db.get_item_type(item_id)
+            item_type = item_type_convert(item_type)
             for condition in ['N', 'U']:
-                data = self.get_price_data(item_id, condition)
+                data = self.get_price_data(item_id, condition, item_type)
                 self.insert_price_info(data)
 
+@timer
+def main():
+    update = Update()
+    update.parse_ids()
 
-update = Update()
-update.parse_ids()
+
+if __name__ == '__main__':
+    main()
