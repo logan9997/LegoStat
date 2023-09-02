f2232f27a8a062da706b0a213a93056262df63a3
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-09-02 17:34:41 +0100

    graph date ranges added, (1w, 1m, 3m ...)

--- App/views/trending.py
+++ App/views/trending.py
@@ -3,7 +3,7 @@ from django.core.handlers.wsgi import WSGIRequest
 from db import DB
 from config import Date, Pages
 from utils import (
-    timer, get_graph_data, get_ordered_item_dates, condence_pages, 
+    timer, GraphData, condence_pages, 
     validate_page
 )
 from ..models import Price, Item
@@ -23,7 +23,6 @@ def trending(request:WSGIRequest):
     metric_filter = request.session.get('metric_filter', 'avg_price__N').split('__')
     metric = metric_filter[0]
     condition = metric_filter[1]
-
     
     trending_items:list[dict] = DB().get_trending_items(
         condition=condition, metric=metric, select_fields=('item_id', 'item_type', 'metric_change')
@@ -51,10 +50,12 @@ def trending(request:WSGIRequest):
         Pages.ItemsPerPage.TRENDING_ITEMS * (current_page - 1) : Pages.ItemsPerPage.TRENDING_ITEMS * current_page
     ]
 
+    graph_metric = metric + '_' + {'N': 'new', 'U': 'used'}[condition]
+    graph_date_range = int(request.session.get('graph_range', 99999))
+
     for item in trending_items:
         
         item['item_name'] = Item.objects.filter(item_id=item['item_id']).values_list('item_name', flat=True)[0]
-
         item['avg_price_new'] = Price.objects.filter(
             item_id=item['item_id'], condition='N'
         ).latest('date').avg_price
@@ -71,12 +72,19 @@ def trending(request:WSGIRequest):
             item_id=item['item_id'], condition='U'
         ).latest('date').total_quantity
 
-        item.update(get_graph_data(Price, item['item_id']))
-        item['graph_dates'] = get_ordered_item_dates(Price, item['item_id'])
+        graph_data = GraphData(Price, item['item_id'], date_range=graph_date_range)
+        item.update(graph_data.get_metrics())
+        item['graph_dates'] = graph_data.get_dates()
+
+        oldest_metric_occurance = item['graph_' + graph_metric][0]
+        newest_metric_occurance = item['graph_' + graph_metric][-1]
+        item['metric_percentage_change'] = round(
+            (oldest_metric_occurance - newest_metric_occurance) / oldest_metric_occurance * -100
+        ,2)
 
     context = {
         'trending_items':trending_items,
-        'metrics':[metric + '_' + {'N': 'new', 'U': 'used'}[condition]],
+        'metrics':[graph_metric],
         'pages':pages,
         'current_page':current_page
     }
