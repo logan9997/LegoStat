61e075b94b081458056466b3725459ee61350fae
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-09-08 20:19:48 +0100

    added zero_pad_float and get_graph_date_range_buttons

--- App/templatetags/tags.py
+++ App/templatetags/tags.py
@@ -1,6 +1,7 @@
 from django import template
 from ..models import Item
 from utils import clean_html_codes
+from config import Options
 
 register = template.Library()
 
@@ -13,6 +14,10 @@ def get_all_item_names():
     names = list(Item.objects.all().values_list('item_name', flat=True).order_by('item_id'))
     names = list(map(clean_html_codes, names))
     return names
+
+@register.simple_tag
+def get_graph_date_range_buttons():
+    return Options.GRAPH_DATE_RANGE_BUTTONS
 
 @register.filter
 def get(_dict, key):
@@ -29,3 +34,12 @@ def index(iterable, index:int):
 @register.filter
 def abs(number:int):
     return number.__abs__()
+
+@register.filter
+def zero_pad_float(number:float):
+    num = str(number)
+    if '.' in num:
+        parted_out = num.split('.')
+        if len(parted_out[1]) <= 1:
+            num = parted_out[0] + '.' + parted_out[1] + '0'
+    return num
