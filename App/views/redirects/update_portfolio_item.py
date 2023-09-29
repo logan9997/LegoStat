f71f73eea0fe7d1f75929d1c58b43fb8a3d2744f
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-09-29 20:12:37 +0100

    functionality to update portfolio item working

--- App/views/redirects/update_portfolio_item.py
+++ App/views/redirects/update_portfolio_item.py
@@ -1,5 +1,15 @@
 from django.shortcuts import redirect
+from django.core.handlers.wsgi import WSGIRequest
 
-def update_portfolio_item(request, item_id:str):
+from ...forms import UpdatePortfolioIten
+from ...models import Portfolio
+
+def update_portfolio_item(request:WSGIRequest, entry_id:int, item_id:str):
+
+    if request.method == 'POST':
+        form = UpdatePortfolioIten(request.POST)
+        if form.is_valid():
+            portfolio_item = Portfolio.objects.filter(entry_id=entry_id)
+            portfolio_item.update(**form.cleaned_data)
 
     return redirect('portfolio_item', item_id) 
