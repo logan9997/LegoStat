63901fd7ffcff29971d91557d7aa6822f634a337
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-09-29 20:10:15 +0100

    created UpdatePortfolioIten form

--- App/forms.py
+++ App/forms.py
@@ -10,3 +10,11 @@ class SignUp(forms.Form):
     username = forms.CharField(max_length=MV.Lengths.USERNAME)
     password = forms.CharField(max_length=MV.Lengths.PASSWORD)
     password_confirmation = forms.CharField(max_length=MV.Lengths.PASSWORD)
+
+
+class UpdatePortfolioIten(forms.Form):
+    date_bought = forms.DateField(required=False)
+    date_sold = forms.DateField(required=False)
+    bought_for = forms.DecimalField(required=False, min_value=0)
+    sold_for = forms.DecimalField(required=False, min_value=0)
+    notes = forms.CharField(required=False, max_length=MV.Lengths.NOTES)
